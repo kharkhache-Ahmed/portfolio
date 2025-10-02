cbd293f6a2172a96f1723d84b3993844b1595342
Ahmed <ahmed@gmail.com>
2025-10-02 19:58:43 +0100

    add a dynamic links underline

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,30 +6,46 @@ const Nav = () => {
     return (
         <div className='fixed top-0 flex justify-between items-center bg-slate-900 shadow-lg w-screen h-20'>
             <a href="/">
-                <img className='
-                    w-24 
-                    grayscale-75 
-                    hover:grayscale-0 
-                    hover:-translate-y-2 
-                    duration-200
-                    ' src={logo} alt="logo" />
+                <img className='w-24 grayscale-75 hover:grayscale-0 hover:-translate-y-1 duration-200
+                ' src={logo} alt="logo" />
             </a>
-            <ul className='flex items-center gap-10 text-white h-full'>
+            <div className='flex items-center gap-10 text-white h-full'>
                 {
                     links.map((e,i)=>{
                         return(
-                            <li key={i} className='
-                            h-full
-                            flex items-center
-                            px-3
-                            hover:cursor-pointer
-                            hover:text-blue-300
-                            duration-200
-                            '><a href={`/${e==="Home"? "":e}`}>{e}</a></li>
+                            <a key={i} className='
+                                group
+                                px-3
+                                hover:cursor-pointer
+                                hover:text-blue-300
+                                duration-200
+                                relative
+                                tracking-wide
+                                h-full
+                                flex
+                                items-center
+                                '
+                                href={`/${e==="Home"? "":e}`}>{e}
+                                <span className='
+                                    absolute
+                                    bottom-0
+                                    left-0
+                                    w-full
+                                    h-[2px]
+                                    bg-blue-500
+                                    scale-x-0
+                                    group-hover:scale-x-100
+                                    origin-right
+                                    group-hover:origin-left
+                                    transition-transform
+                                    ease-in-out
+                                    duration-200
+                                    '></span>
+                            </a>
                         )
                     })
                 }
-            </ul>
+            </div>
             <div className='mr-4 flex justify-center items-center gap-4'>
                 <button className='
                     bg-slate-700
@@ -38,7 +54,7 @@ const Nav = () => {
                     hover:text-white
                     hover:cursor-pointer
                     duration-200
-                    py-1
+                    py-2
                     px-4
                     rounded-lg
                     shadow-lg
