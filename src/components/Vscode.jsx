b27afc4bbe264aa0dd189b63c5f51326e1f0afeb
Ahmed <ahmed@gmail.com>
2025-10-13 19:21:19 +0100

    apply dark and light mode

--- src/components/Vscode.jsx
+++ src/components/Vscode.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const Vscode = () => {
+const Vscode = ({notDark,setNotDark}) => {
     return (
         <>
 
         {/* Top side of vsCode style */}
-        <div className='
+        <div className={`
             rounded-t-xl
             flex
             items-center
             gap-2
             px-4
-            h-10 
+            h-8
             w-full 
-            bg-gray-950/70'>
-            <span className='bg-red-500/70 vsCodeCircle'></span>
-            <span className='bg-yellow-500/70 vsCodeCircle'></span>
-            <span className='bg-green-500/70 vsCodeCircle'></span>
-            <span className='text-white text-sm'>My story</span>
+            bg-gray-950/70`}>
+            <span className='bg-red-500 vsCodeCircle'></span>
+            <span className='bg-yellow-500 vsCodeCircle'></span>
+            <span className='bg-green-500 vsCodeCircle'></span>
+            <span className='text-sm'>My story</span>
         </div>
 
         {/* vsCode Divider line style */}
@@ -38,11 +38,11 @@ const Vscode = () => {
             p-4
         '>
             <p className='h-full font-semibold text-white' >
-                <span className='text-red-500'>
+                <span className='text-red-400'>
                     const </span>
                 aboutMe <span className='text-green-500'>
                 =</span> <span
-                className='text-cyan-400'
+                className='text-cyan-300'
                 >
                 " I'm Ahmed Kharkhache, a Full Stack Web Developer driven by a passion for creating clean, efficient, and visually engaging web solutions. I work across the stack — building dynamic front-ends with React and Tailwind CSS, and powerful back-ends using Node.js, Express, or Laravel. I love turning ideas into functional, high-performing applications that deliver real value and seamless user experiences. "
                 </span> ;
